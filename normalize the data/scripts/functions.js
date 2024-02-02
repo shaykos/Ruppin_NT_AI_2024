101bad869c15cca3c0b5e8cada2dc76fafdb6dba
Shay Abraham <shaykos@gmail.com>
2024-02-02 12:15:34 +0200

    Update functions.js

--- normalize the data/scripts/functions.js
+++ normalize the data/scripts/functions.js
@@ -20,28 +20,35 @@ export function createNN() {
         { input: { temperature: 10, humidity: 40, wind: 5 }, output: { goForRun: 0 } },
         { input: { temperature: 20, humidity: 55, wind: 12 }, output: { goForRun: 1 } },
         { input: { temperature: 15, humidity: 60, wind: 8 }, output: { goForRun: 0 } },
-        { input: { temperature: 18, humidity: 50, wind: 5 }, output: { goForRun: 0 } },
+        { input: { temperature: 18, humidity: 50, wind: 5 }, output: { goForRun: 0 } }
     ];
 
-    let newData = normalizeData(trainingData);
+    let noramlData = normalizeData(trainingData);
 
     // Train the neural network
-    net.train(newData, {
+    net.train(noramlData, {
         logPeriod: 100,
-        log: (stats) => console.log(stats)
+        log: (stats) => console.log(stats),
+        //learningRate: 0.1,
+        //errorThresh: 0.01,
     });
 
     // Make predictions
-    const decision1 = net.run(normalizeInput({ temperature: 33, humidity: 70, wind: 12 }));
+    const decision1 = net.run(normalizeInput({ temperature: 30, humidity: 70, wind: 12 }));
     // Expected output: { goForRun: 1 }
 
-    const decision2 = net.run(normalizeInput({ temperature: 10, humidity: 40, wind: 5 }));
+    const decision2 = net.run(normalizeInput({ temperature: 10, humidity: 70, wind: 12 }));
     // Expected output: { goForRun: 0 }
+
+    const decision3 = net.run(normalizeInput({ temperature: 25, humidity: 60, wind: 10 }));
+    // Expected output: { goForRun: 1 }
 
     // Display predictions
     console.log('Decision for {30, 70, 12}:', decision1);
-    console.log('Decision for {10, 40, 5}:', decision2);
+    console.log('Decision for {10, 70, 12}:', decision2);
+    console.log('Decision for {25, 60, 10}:', decision3);
 
+    //Display neural network
     document.querySelector('#app').innerHTML = brain.utilities.toSVG(net);
 
 }
@@ -68,9 +75,9 @@ function normalizeInput(input) {
     //wind -> 20% --> 0.2
     // { input: { temperature: 25, humidity: 60, wind: 10 }, output: { goForRun: 1 } },
 
-    input.temperature = input.temperature * 0.5
-    input.humidity = input.humidity * 0.3
-    input.wind = input.wind * 0.2
+    input.temperature *= 0.5
+    input.humidity *= 0.3
+    input.wind *= 0.2
 
     return input;
 }
